55cafb4a3dd524724f7fbaed793a46642aa392b0
Frank <wanyoike39@gmail.com>
2024-09-26 14:47:07 +0300

    add boilerplate code

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+    return (
+        <div className="App">
+            <h1>the app works!</h1>
+        </div>
+            )
+}
+
+export default App;
